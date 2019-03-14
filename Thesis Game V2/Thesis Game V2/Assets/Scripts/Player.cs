3705610f560cd1c5cddce21b292a20e2aca715d4
darrencam12 <34488454+darrencam12@users.noreply.github.com>
2019-03-14 21:09:49 +0100

    Created The Game
    Added the player movement and some of the main functionality

---
+++ Thesis Game V2/Thesis Game V2/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float movementSpeed;
+    public Rigidbody theRB;
+    public float JumpForce;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * movementSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * movementSpeed);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            theRB.velocity = new Vector3(theRB.velocity.x, JumpForce, theRB.velocity.z);
+        }
+    }
+}
---
+++ Thesis Game V2/Thesis Game V2/Assets/Scripts/PuzzleManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour
+{
+    public int wordLength;
+    public GameObject cubePos;
+    public GameObject letter;
+    public Transform intialPos;
+    public Vector3 latestPos;
+    private List<Vector3> positions;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+        //Debug.Log(wordLength);
+        
+    }
+    void Start()
+    {
+        positions = new List<Vector3>();
+        for (int i = 0; i < wordLength; i++)
+        {
+            Instantiate(cubePos, new Vector3(intialPos.position.x + i, intialPos.position.y, intialPos.position.z), Quaternion.identity);
+            latestPos = new Vector3(intialPos.position.x + i, intialPos.position.y, intialPos.position.z);
+            positions.Add(latestPos);
+        }
+
+        foreach(Vector3 cu in positions)
+        {
+            Instantiate(letter, new Vector3(cu.x, cu.y, cu.z),Quaternion.identity);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Add()
+    {
+
+    }
+
+}
