6a8a2f46f05bde194574e002068e2f7e853fc150
darrencam12 <34488454+darrencam12@users.noreply.github.com>
2019-04-09 00:06:17 +0200

    Found and created the bridge mechanic

---
+++ ScabbleWord/Assets/Word Scramble/Script/WordScramble.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Word{
+    public string word;
+    [Header("leave empty if you want randmized")]
+    public string desiredRandom;
+    public string GetString()
+    {
+        if(!string.IsNullOrEmpty(desiredRandom))
+        {
+            return desiredRandom;
+        }
+        string result = word;
+        
+        while (result == word)
+        {
+            result = "";
+            List<char> characters = new List<char>(word.ToCharArray());
+            while (characters.Count > 0)
+            {
+                int indexChar = Random.Range(0, characters.Count - 1);
+                result += characters[indexChar];
+
+                characters.RemoveAt(indexChar);
+            }
+        }
+        return result;
+    }
+}
+public class WordScramble : MonoBehaviour
+{
+    public Word[] words;
+
+    [Header("UI REFERENCE")]
+    public CharObject prefab;
+    public Transform container;
+    public float space;
+
+    public float lerpspeed = 5;
+
+
+    List<CharObject> charObjects = new List<CharObject>();
+    CharObject firstSelected;
+    public int currentWord;
+    public static WordScramble main;
+
+    void Awake()
+    {
+        main = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowScramble(currentWord);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RepostionObject();
+    }
+
+    void RepostionObject()
+    {
+        if (charObjects.Count == 0)
+        {
+            return;
+        }
+        float center = (charObjects.Count -1) / 2;
+        for (int i = 0; i < charObjects.Count; i++)
+        {
+            charObjects[i].rectTransform.anchoredPosition = Vector2.Lerp(charObjects[i].rectTransform.anchoredPosition, new Vector2((i - center)* space, 0), lerpspeed * Time.deltaTime);
+            charObjects[i].index = i;
+        }
+        
+    }
+    
+    // show a random word to the screen
+    public void ShowScramble()
+    {
+        ShowScramble(Random.Range(0, words.Length - 1));
+    }
+    /*
+        show word for collecton with desired index
+        param index = index of the elemnt
+     */
+    public void ShowScramble(int index)
+    {
+        charObjects.Clear();
+        foreach(Transform child in container)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // WORDS FINISHED
+        if (index > words.Length - 1)
+        {
+            Debug.LogError("index out of range, please enter range between 0-"+ (words.Length -1).ToString());
+            return;
+        }
+        char[] chars = words[index].GetString().ToCharArray();
+        foreach(char c in chars)
+        {
+            CharObject clone = Instantiate(prefab.gameObject).GetComponent<CharObject>();
+            clone .transform.SetParent(container);
+
+            charObjects.Add(clone.Init(c));
+        }
+        currentWord = index;
+    }
+
+    public void Swap (int indexA, int indexB)
+    {
+        CharObject tmpA = charObjects[indexA];
+
+        charObjects[indexA] = charObjects[indexB];
+        charObjects[indexB] = tmpA;
+
+        charObjects[indexA].transform.SetAsLastSibling();
+        charObjects[indexB].transform.SetAsLastSibling();
+
+        CheckWord();
+    }
+    
+
+    public void Select (CharObject charObject)
+    {
+        if(firstSelected)
+        {
+            Swap(firstSelected.index, charObject.index);
+
+            //unselect
+            firstSelected.Select();
+            charObject.Select();
+            
+        }else
+        {
+            firstSelected = charObject;
+        }
+    }
+
+    public void UnSelect()
+    {
+        firstSelected = null;
+    }
+    public void CheckWord()
+    {
+        StartCoroutine(CoCheckWord());
+    }
+
+    IEnumerator CoCheckWord()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        string word = "";
+        foreach (CharObject charObject in charObjects)
+        {
+            word += charObject.character;
+        }
+        if(word == words[currentWord].word)
+        {
+            currentWord++;
+            ShowScramble(currentWord);
+            
+        }
+       
+    }
+}
